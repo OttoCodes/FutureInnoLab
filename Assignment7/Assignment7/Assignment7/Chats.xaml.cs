401efbfe2a91c449c91eeb93ca4e31f036492542
spiritsage <62544132+spiritsage@users.noreply.github.com>
2020-11-12 06:51:15 +0200

    first

---
+++ Assignment7/Assignment7/Assignment7/Chats.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Assignment7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Chats : ContentPage
+    {
+        public Chats()
+        {
+            InitializeComponent();
+        }
+
+        private void Contact_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
